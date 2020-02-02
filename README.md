515ab6d1346daa64e9bbf8101c4e74507ab00689
zxltrn <zaltron.julian@gmail.com>
2020-02-01 22:37:08 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# MeLi Api Data Retriever
+
+This tool allows you to fetch product data from Mercado Libre's public API. 
+
+## Getting Started
+### Pre-requisites
+Install the following before using this tool:
+```
+Python 3.0 or later.
+pip3 (sudo apt-get install python3-pip).
+```
+
+### Installing
+```bash
+$ git clone https://github.com/juzalt/meli-api-data-retriever.git
+$ cd meli-api-data-retriever
+$ pip3 install -r requirements.txt
+```
+
+### Parameters
+```
+-r --region [country] (required)
+-q --query [search query] (required)
+-o --output [output file] (optional)
+```
+
+### Usage
+```bash
+$ python3 main.py -r <region> -q <search query> [-o outputFile.txt]
+```
+### Example
+```bash
+$ Example: python3 main.py -r MLA -q samsung_tv -o argentine-samsung-tv.txt
+```
+
+## Screenshots
+<p align="center">
+  <img src="https://julianzaltron.now.sh/static/media/meliadr-argentina.c371fd36.png" alt="Screenshot showing command being run for Argentina." />
+</p>
+
+<p align="center">
+  <img src="https://julianzaltron.now.sh/static/media/meliadr-mexico.2235e3fc.png" alt="Screenshot showing command being run for Mexico." />
+</p>
+
+## Author
+[(Julian Zaltron)](https://julianzaltron.now.sh)
---
+++ main.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+------------------------------------------------------------------------------
+	MeLi-api-data-retriever - 01.02.20 - Julian Zaltron (juzalt)
+------------------------------------------------------------------------------
+"""
+
+## # LIBRARIES # ##
+import requests, json
+
+## # CONTEXT VARIABLES # ##
+version = 1.0
+
+## # MAIN FUNCTIONS # ##
+def banner():
+	global version
+	b = '''
+         ___   ____   __
+        | _ \_/   |  | |      
+        | |\   /| |  | |      
+        | | \_/ | |  | |____ 
+        |_|     |_|e |______|i Api Data Retriever 
+	
+    Version {v}
+    Made by Julian Zaltron (juzalt)
+
+    Usage: python3 main.py -r <region> -q <search query> [-o outputFile.txt]
+    Example: python3 main.py -r MLA -q samsung_tv -o argentine-samsung-tv.txt
+
+    Check for possible regions @ https://api.mercadolibre.com/sites/
+	'''.format(v=version)
+	print(b)
+
+def parse_args():
+  import argparse
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-o', '--output', type=str, help="Output file.")
+  parser.add_argument('-r', '--region', type=str, required=True, help="Region.")
+  parser.add_argument('-q', '--query', type=str, required=True, help="Search Query.")
+  return parser.parse_args()
+
+def save_products(product,output_file):
+  fileContent = json.dumps(product)
+  with open("./results/{o}".format(o=output_file),"a") as file:
+    file.write(fileContent)
+    file.close()
+
+def main():
+  banner()
+  args = parse_args()
+
+  products = []
+  output = args.output
+  query = args.query
+  siteID = args.region
+
+  req = requests.get("https://api.mercadolibre.com/sites/{}/search?q={}".format(siteID, query))
+
+  if req.status_code != 200:
+    print("Something went wrong. Check https://status.mercadolibre.com/")
+    exit(1)
+
+  response_data = req.json()
+
+  for product in response_data["results"]:
+    products.append(product)
+
+  for product in products:
+    print("\n {p}".format(p=product))
+    if output is not None:
+      save_products(product,output)
+
+  print("\n\n That's all folks!")
+
+main()
